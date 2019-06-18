9a594c8d39a9221ebc950c118d791d44568660b6
cjbi <cjbi@outlook.com>
2019-06-18 17:36:16 +0800

    代码提交

--- weshop-user/src/main/java/tech/wetech/weshop/config/SecurityConfig.java
+++ weshop-user/src/main/java/tech/wetech/weshop/config/SecurityConfig.java
@@ -1,14 +1,12 @@
 package tech.wetech.weshop.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import tech.wetech.weshop.service.impl.UserDetailsServiceImpl;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsServiceImpl userDetailService;
